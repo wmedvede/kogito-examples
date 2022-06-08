f770eed3b35de8aa84002a75876673e1f6d5d64b
Walter Medvedeo <wmedvedeo@gmail.com>
2022-06-08 18:27:13 +0200

    KOGITO-7263: Version I

---
+++ kogito-quarkus-examples/serverless-workflow-oauth2-orchestration-quarkus/eu-central-bank-service/src/main/java/com/eu/central/bank/EUCentralBankResource.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.eu.central.bank;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+
+@Path("central-bank")
+public class EUCentralBankResource {
+
+    @GET
+    @Path("reference-rates")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "referenceRates")
+    @SecurityRequirement(name = "eu-central-bank-oauth")
+    public ReferenceRates getReferenceRates(@QueryParam("date") String date) {
+
+        List<ReferenceRate> result = new ArrayList<>();
+        result.add(new ReferenceRate(LocalDate.parse(date), "USD", 1.0726f));
+        result.add(new ReferenceRate(LocalDate.parse(date), "JPY", 140.16f));
+        return new ReferenceRates(result);
+    }
+}
---
+++ kogito-quarkus-examples/serverless-workflow-oauth2-orchestration-quarkus/exchange-workflow/src/main/java/org/kie/kogito/examples/CalculateExchangeService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.examples;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@ApplicationScoped
+public class CalculateExchangeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculateExchangeService.class);
+
+    public ExchangeResult calculateExchange(String currencyFrom, String currencyTo, double amount, List<JsonNode> rates) {
+
+        LOGGER.debug("Calculating exchange, currencyFrom: {}, currencyTo: {}, amount: {}, rates: {}",
+                currencyFrom, currencyTo, amount, rates);
+
+        if (amount <= 10) {
+            return new ExchangeResult(1234d);
+        } else {
+            return new ExchangeResult(null);
+        }
+    }
+
+}
